f4d5d3d1598cf9b26a5e32d6849b4eda8ff7aaea
AGrosserHH <agrossi@gmx.de>
2020-10-06 10:38:26 +0200

    new exercises

--- rustlingsmysolutions/exercises/if/if1.rs
+++ rustlingsmysolutions/exercises/if/if1.rs
@@ -1,7 +1,5 @@
 // if1.rs
 
-// I AM NOT DONE
-
 pub fn bigger(a: i32, b: i32) -> i32 {
     // Complete this function to return the bigger number!
     // Do not use:
@@ -11,7 +9,7 @@ pub fn bigger(a: i32, b: i32) -> i32 {
    if a>b{
 	a
 }
-   else { 
+   else {
 	b
    }
 }
